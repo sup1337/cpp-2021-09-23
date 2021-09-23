db54833a934feda7aa5ee50143d521a648ebbda5
laszlo.hunor <laszlo_huni@yahoo.com>
2021-09-23 13:15:08 +0300

    second3

---
+++ lab01/functions.cpp
@@ -0,0 +1,4 @@
+//
+// Created by laszlo.hunor on 9/23/2021.
+//
+#include "functions.h"
---
+++ lab01/functions.h
@@ -0,0 +1,8 @@
+//
+// Created by laszlo.hunor on 9/23/2021.
+//
+
+#ifndef CPP_2021_FUNCTIONS_H
+#define CPP_2021_FUNCTIONS_H
+
+#endif //CPP_2021_FUNCTIONS_H
