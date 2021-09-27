05f3358cb0577aa3889bf8cbf9c8c5f560586723
sup1 <laszlosupihunor@yahoo.com>
2021-09-27 19:33:52 +0300

    .

--- lab01/functions.cpp
+++ lab01/functions.cpp
@@ -1,6 +1,7 @@
 //
 // Created by laszlo.hunor on 9/23/2021.
 //
+#include <iostream>
 #include "functions.h"
 int countBits( int number) {
     int count = 0;
@@ -12,5 +13,17 @@ int countBits( int number) {
     return count;
 }
 bool setBits(int& number , int order){
+    if(order>0&&order<31){
+        int counter=0;
+        while (counter!=order){
+            number=number>>1;
+            ++counter;
+        }
+        number=number|(1<<order);
+        std::cout<< "returned number is:" << number <<std::endl;
+        return true;
+    } else{
+        return false;
+    }
 
 }
