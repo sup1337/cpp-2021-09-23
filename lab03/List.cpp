0e81988ec2a433f9db96145424b34e03cd850042
sup1 <laszlosupihunor@yahoo.com>
2021-10-07 13:57:50 +0300

    .

---
+++ lab03/List.cpp
@@ -0,0 +1,72 @@
+//
+// Created by Laszlo Hunor on 07.10.2021.
+//
+
+#include "List.h"
+#include <iostream>
+using namespace std;
+
+List::List() {
+    //cout<<"constructor"<<endl;
+    this->first= nullptr;
+}
+
+List::~List() {
+    //cout<<"destructor"<<endl;
+    while (first!= nullptr){
+        Node*temp=first;
+        first=first->next;
+        delete temp;
+    }
+    nodeCounter=0;
+}
+
+bool List::exists(int d) const {
+    int i=0;
+    Node*temp=first;
+    while (i!=nodeCounter)
+    {
+        if(temp->value==d){
+            return true;
+        }
+        temp=temp->next;
+        i++;
+    }
+    return false;
+}
+
+int List::size() const {
+    cout<<nodeCounter;
+    return 0;
+}
+
+bool List::empty() const {
+    if(first== nullptr){
+        return true;
+    }
+    return false;
+}
+
+void List::insertFirst(int d) {
+    first=new Node(d, first);
+    ++nodeCounter;
+}
+
+int List::removeFirst() {
+    return 0;
+}
+
+void List::remove(int d, List::DeleteFlag df) {
+
+}
+
+void List::print() const {
+    Node* it=first;
+    cout<<"[";
+    while (it!= nullptr)
+    {
+        cout<<it->value<<" ";
+        it=it->next;
+    }
+    cout<<"]"<<endl;
+}
--- lab03/main.cpp
+++ lab03/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include "List.h"
 
+using namespace std;
 int main() {
     std::cout << "Hello, lab3" << std::endl;
-
+    List list;
+    for (int i = 0; i < 10; ++i) {
+        list.insertFirst(i);
+        list.print();
+        list.empty();
+    }
     return 0;
 }
