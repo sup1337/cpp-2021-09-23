9515d15e9f7d6c0da0a04400a2b17e8e3771edf3
sup1 <laszlosupihunor@yahoo.com>
2021-12-09 15:59:47 +0200

    lab10

---
+++ lab10/cmake-build-debug/CMakeFiles/lab10.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/sup1/CLionProjects/cpp-2021-09-23/lab10/GraduationExam.cpp" "/Users/sup1/CLionProjects/cpp-2021-09-23/lab10/cmake-build-debug/CMakeFiles/lab10.dir/GraduationExam.cpp.o"
+  "/Users/sup1/CLionProjects/cpp-2021-09-23/lab10/Student.cpp" "/Users/sup1/CLionProjects/cpp-2021-09-23/lab10/cmake-build-debug/CMakeFiles/lab10.dir/Student.cpp.o"
+  "/Users/sup1/CLionProjects/cpp-2021-09-23/lab10/main.cpp" "/Users/sup1/CLionProjects/cpp-2021-09-23/lab10/cmake-build-debug/CMakeFiles/lab10.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab10/cmake-build-debug/CMakeFiles/lab10.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab10.dir/GraduationExam.cpp.o"
+  "CMakeFiles/lab10.dir/Student.cpp.o"
+  "CMakeFiles/lab10.dir/main.cpp.o"
+  "lab10"
+  "lab10.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
